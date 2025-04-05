935ca8ca935e59b5b858ef4c447d0668d6bb6cd8
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-04-05 10:44:21 +0530

    routes update

--- express-project/routes/contactRoutes.js
+++ express-project/routes/contactRoutes.js
@@ -9,14 +9,19 @@ const {
   deleteContact,
 } = require("../controller/contactController");
 
-router.route("/").get(getContacts);
+router.route("/")
+  .get(getContacts)
+  .post(createContact);
 
-router.route("/").post(createContact);
+router.route('/:id')
+  .get(getContact)
+  .put(updateContact)
+  .delete(deleteContact);
 
-router.route("/:id").put(updateContact);
+// router.route("/:id").put(updateContact);
 
-router.route("/:id").get(getContact);
+// router.route("/:id").get(getContact);
 
-router.route("/:id").delete(deleteContact);
+// router.route("/:id").delete(deleteContact);
 
 module.exports = router;
